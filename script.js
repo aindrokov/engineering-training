3bf9f46dd9dc0dd157498ec3ea20b02a5733dd98
Artem Indrokov <aindrokov@totalwine.com>
2023-01-26 13:17:08 -0500

    TT-19

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 console.log("Engineering Training!");
 
-const modalButton = document.getElementById('modalButton');
-const modalContainer = document.getElementById('modalContainer');
+const modalButton = document.getElementById("modalButton");
+const modalContainer = document.getElementById("modalContainer");
+const closeModalButton = document.getElementsByClassName("close-modal-button");
 
 modalButton.addEventListener("click", function () {
-  console.log("clicked button!");
-  const result = modalContainer.classList.toggle("hidden");
+  console.log("Clicked Button!");
+  modalContainer.classList.toggle("hidden");
 });
 
-console.log("modalButton ", modalButton);
+console.log("closeModalButton", closeModalButton);
