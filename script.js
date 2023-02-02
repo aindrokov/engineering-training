b39b0955dc9662b1f8ab2c61b04983b672c7cc2d
Artem Indrokov <aindrokov@totalwine.com>
2023-02-02 11:13:09 -0500

    TT-20

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const modalButton = document.getElementById("modalButton");
 const modalContainer = document.getElementById("modalContainer");
 const closeModalButton = document.getElementsByClassName("close-modal-button");
 
+const jiraTitles = [
+  "Create a public repository under your GitHub account",
+  "Create a new script file, and import it into index.html and add a console log",
+  "JavaScript: Variables",
+  "JavaScript: Event Listeners - Add Toggle Button Inside of Modal",
+  "JavaScript: Functions - Write a function to toggle hidden class on modal",
+];
+console.log("jiraTitles", jiraTitles);
+
+const jiraLinks = [
+  "https://totalwine.atlassian.net/browse/TT-2",
+  "https://totalwine.atlassian.net/browse/TT-16",
+  "https://totalwine.atlassian.net/browse/TT-17",
+  "https://totalwine.atlassian.net/browse/TT-18",
+  "https://totalwine.atlassian.net/browse/TT-19",
+];
+console.log("jiraLinks", jiraLinks);
+
 modalButton.addEventListener("click", function () {
   console.log("Clicked Button!");
   modalContainer.classList.toggle("hidden");
