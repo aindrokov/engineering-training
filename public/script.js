6b6723b0bb26b66ff0fca1e63616de933cffa1d4
Artem Indrokov <aindrokov@totalwine.com>
2023-09-11 13:34:32 -0400

    TT-140-ResolvingErrors

--- public/script.js
+++ public/script.js
@@ -25,33 +25,32 @@ function initModalButton() {
 }
 
 const utils = {
+  loadData: async function (callback) {
+    const response = await fetch("/dataHandler");
+    const data = await response.json();
+    console.log(data);
+
+    this.renderData(data).then((response) => {
+      dataLoaded = true;
+      listElement[0].innerHTML = response;
+      modalContainer.classList.toggle("hidden");
+      console.log("data loaded");
+      return response;
+    });
+    callback();
+  },
   renderData: function (data) {
     return new Promise((resolve) => {
       let response = "";
       data.jirasObject.forEach((object) => {
-        const { link, title, icon } = object;
-        response += `<li>
-            <i class="${icon}"></i>
-            <a href="${link}">${title}</a>
-          </li>`;
+        let { link, title, icon } = object;
+        response += `<li class="item"><a href= ${link}> 
+      <i class="${icon}">
+      </i> ${title} 
+      </a></li>`;
       });
       resolve(response);
     });
-  },
-  loadData: async function (callback) {
-    const response = await fetch("/dataHandler");
-    const data = await response.json();
-    console.log("data", data);
-    setTimeout(function () {
-      utils.renderData().then((response) => {
-        dataLoaded = true;
-        listElement[0].innerHTML = response;
-        modalContainer.classList.toggle("hidden");
-        console.log("data loaded");
-        return response;
-      });
-    }, 1000);
-    callback();
   },
 };
 
