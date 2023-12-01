c826a3ea24ebab13c34a6aa9655d1006c34bb8df
Artem Indrokov <aindrokov@totalwine.com>
2023-12-01 08:20:21 -0500

    TT-152

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import utils from "../utils";
 
 export default class Button extends React.Component {
-    render() {
-      return <button>Load Data</button>;
-    }
+  constructor(props) {
+    super(props);
+    this.state = { dataLoaded: false };
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
+
+  handleOnClick() {
+    console.log("Clicked!");
+    const modalContainer = document.getElementById("modalContainer");
+    if (this.state.dataLoaded) {
+      return;
+    }
+    modalContainer.classList.toggle("hidden");
+
+    utils.loadData(() => {
+      this.setState({ dataLoaded: true });
+      resolve();
+    });
+  }
+  render() {
+    return (
+      <button onClick={this.handleOnClick}>
+        {this.state.dataLoaded ? "LOADED" : "LOAD"}
+      </button>
+    );
+  }
+}
