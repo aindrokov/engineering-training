18a218b958cdfb06b250d2d75e5b7d6ef3e3d80a
Artem Indrokov <aindrokov@totalwine.com>
2023-12-08 09:58:32 -0500

    TT-152

--- src/components/button.js
+++ src/components/button.js
@@ -1,30 +1,31 @@
 import React from "react";
 import utils from "../utils";
+import store from "../store";
 
 export default class Button extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { dataLoaded: false };
     this.handleOnClick = this.handleOnClick.bind(this);
   }
 
   handleOnClick() {
+    const dataLoaded = store.getState().dataLoaded;
     console.log("Clicked!");
     const modalContainer = document.getElementById("modalContainer");
-    if (this.state.dataLoaded) {
+    if (dataLoaded) {
       return;
     }
     modalContainer.classList.toggle("hidden");
 
     utils.loadData(() => {
-      this.setState({ dataLoaded: true });
+      store.dispatch({type:"TOGGLE_DATALOADED"})
       resolve();
     });
   }
   render() {
     return (
       <button onClick={this.handleOnClick}>
-        {this.state.dataLoaded ? "Loaded" : "Open Modal"}
+        Load Data
       </button>
     );
   }
--- src/script.js
+++ src/script.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Button from './components/button';
 import utils from './utils';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(
-  <Button/>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <Button/>
+  </Provider>
 );
 
 (async function() {
